3841addb5d79c50ebc0e79103a7bc03c41353948
Mahesh Kamble <mahesh.kamble@deptagency.com>
2026-09-07 19:34:23 +0530

    cards css and rte css added

---
+++ blocks/rte/rte.js
@@ -0,0 +1,11 @@
+export default function decorate(block) {
+    if(block.classList.contains('explore-cta')){
+        const buttoncta = block.querySelector('a');
+        if(buttoncta){
+            const buttonContainer = buttoncta.closest('p');
+        if(buttonContainer){
+            buttonContainer.classList.add('cta-container');
+        }
+        }  
+    }
+}
