130766a77fa279e70538ed4d1e04b5893061af76
Alok Singh <alok.singh@deptagency.com>
2026-09-09 14:06:33 +0530

    code add custom cards

--- blocks/custom-cards/custom-cards.js
+++ blocks/custom-cards/custom-cards.js
@@ -1,8 +1,77 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture, loadCSS, loadScript } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-export default function decorate(block) {
-  /* change to ul, li */
+/**
+ * Initializes Swiper instance for mobile viewports (< 900px)
+ * @param {Element} block The custom-cards block element
+ */
+function initInsightsSwiper(block) {
+  const ul = block.querySelector('ul');
+  if (!ul) return;
+
+  // Create pagination container if it doesn't exist
+  let pagination = block.querySelector('.swiper-pagination');
+  if (!pagination) {
+    pagination = document.createElement('div');
+    pagination.className = 'swiper-pagination';
+    block.append(pagination);
+  }
+
+  const mobileQuery = window.matchMedia('(max-width: 899px)');
+
+  const enableSwiper = () => {
+    if (!block.swiperInstance && window.Swiper) {
+      block.classList.add('swiper');
+      ul.classList.add('swiper-wrapper');
+      [...ul.children].forEach((li) => li.classList.add('swiper-slide'));
+
+      block.swiperInstance = new window.Swiper(block, {
+        slidesPerView: 1.18,
+        spaceBetween: 16,
+        grabCursor: true,
+        pagination: {
+          el: pagination,
+          clickable: true,
+        },
+      });
+    }
+  };
+
+  const disableSwiper = () => {
+    if (block.swiperInstance) {
+      block.swiperInstance.destroy(true, true);
+      block.swiperInstance = null;
+      block.classList.remove('swiper');
+      ul.classList.remove('swiper-wrapper');
+      [...ul.children].forEach((li) => li.classList.remove('swiper-slide'));
+    }
+  };
+
+  const handleMediaChange = () => {
+    if (mobileQuery.matches) {
+      enableSwiper();
+    } else {
+      disableSwiper();
+    }
+  };
+
+  handleMediaChange();
+  mobileQuery.addEventListener('change', handleMediaChange);
+}
+
+export default async function decorate(block) {
+  const section = block.closest('.insights-impact-plans');
+  const codeBase = window.hlx?.codeBasePath || '';
+
+  // Load Swiper CSS and JS at top level if block is inside .insights-impact-plans
+  if (section) {
+    await Promise.all([
+      loadCSS(`${codeBase}/blocks/swiper/swiper-bundle.min.css`),
+      loadScript(`${codeBase}/blocks/swiper/swiper-bundle.min.js`),
+    ]);
+  }
+
+  /* Transform row structure to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
@@ -39,4 +108,9 @@ export default function decorate(block) {
 
   block.textContent = '';
   block.append(ul);
+
+  // Initialize Swiper after DOM setup
+  if (section) {
+    initInsightsSwiper(block);
+  }
 }
