a6988c518fdf0bb794366ee8efcd14875904811e
prabhunrayan <Prabhunarayan@teknopoint.local>
2026-09-19 19:23:46 +0530

    add image

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -151,6 +151,7 @@ async function loadEager(doc) {
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
+
     document.body.classList.add('appear');
     await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
